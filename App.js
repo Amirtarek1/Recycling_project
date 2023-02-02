dd826d516d409bbc149ddaa389ae35707e4cdace
Ahmed Tantawy <aaah0093@gmail.com>
2023-02-02 08:51:32 -0800

    modificcation before log im

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 
 import { Text, View } from 'react-native';
 import { COLORS } from './src/constants/index';
-import Large_button from "./src/components/Large_button"
-import Back_arrow from "./src/components/Back_arrow"
-import Signup_page1 from "./src/screens/Auth/Signup_page1"
-import INputbutton from './src/components/INputbutton';
+
+
 const App = () => {
 
 
   return (
     <>
 
-{/* <Large_button button_name = "انشاء الحساب"/> */}
-{/* <INputbutton/> */}
-{/* <Back_arrow/> */}
-<Signup_page1/>
+
+      <View>
+        <Text style = {{ fontFamily : "NotoSansArabic-VariableFont_wdth,wght"  
+        , fontSize :30}}>سشبيشؤش</Text>
+      </View>
+
+
+
     </>
   )
 
