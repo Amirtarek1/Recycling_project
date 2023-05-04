2a3775fc29d045a9f58b03a4517da276c8023d0c
Amir tarek <amir.tarek8880@gmail.com>
2023-05-04 15:31:18 +0300

    add modified

--- src/screens/Home/Home_page.js
+++ src/screens/Home/Home_page.js
@@ -92,7 +92,7 @@ const Home_page = () => {
 
                     <View style={styles.view_photo_and_text_style}>
                         <User_image />
-                        <View style = {{flex : 1 ,marginLeft: RFPercentage(1)}} >
+                        <View  >
                             <Text style={styles.text_Bold_style}>مرحبا دكتور أسامه </Text>
                             <Text style={styles.text_thin_style}>النقط : 100</Text>
 
